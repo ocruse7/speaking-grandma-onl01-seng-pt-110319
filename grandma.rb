2a2c15fc9ac629b118a144924d72646ee0b5e8d0
Owen Cruse <ocruse7@gmail.com>
2019-11-09 23:01:17 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,10 +14,10 @@
 
 def speak_to_grandma( phrase )
   if ( phrase == phrase.upcase )
-    pp "NO, NOT SINCE 1938!"
+    return "NO, NOT SINCE 1938!"
   elsif ( phrase == "I LOVE YOU GRANDMA!" )
-    pp "I LOVE YOU TOO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
   else
-    pp "HUH? SPEAK UP< SONNY!"
+    return "HUH? SPEAK UP< SONNY!"
   end
 end
