cc505c8cccdad5ac5a407ad88b64d9b330756f77
Owen Cruse <ocruse7@gmail.com>
2019-11-09 23:01:43 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -18,6 +18,6 @@ def speak_to_grandma( phrase )
   elsif ( phrase == "I LOVE YOU GRANDMA!" )
     return "I LOVE YOU TOO PUMPKIN!"
   else
-    return "HUH? SPEAK UP< SONNY!"
+    return "HUH? SPEAK UP, SONNY!"
   end
 end
